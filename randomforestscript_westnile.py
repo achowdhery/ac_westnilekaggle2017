2e1c55ed7e13cb39265a931643373d6c2c44ef95
achowdhery <aakankshachowdhery@gmail.com>
2017-09-26 13:52:49 -0400

    Update randomforestscript_westnile.py

--- randomforestscript_westnile.py
+++ randomforestscript_westnile.py
@@ -1,210 +1,272 @@
-import numpy as np
-import csv
+# This script contains functions used for data loading, cleaning, merging, feature engineering,
+# and saving predictions
+# It also contains a stacking function, used to obtain meta-features: predicted log(number of mosquitos) for the 2d stage
+# Finally, there is a bagging (bootstrap) function designed to stabilize predictions of the Neural Net
+# classifier
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
+import csv
 import pandas as pd
+import numpy as np
+from math import radians, sin, cos, sqrt, asin
+
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+
+#import sklearn.metrics as metrics
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn import learn_io
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
+from tensorflow import losses
 from sklearn import metrics
 from sklearn.cross_validation import KFold
 from sklearn.preprocessing import StandardScaler
 
-from matplotlib import cm
-from matplotlib import gridspec
-from matplotlib import pyplot as plt
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
 
 '''
     Kaggle West Nile virus prediction challenge.
 
 '''
 
-# process weather data
-def get_weather_data():
-    weather_dic = {}
-    fi = csv.reader(open("weather.csv"))
-    weather_head = fi.next()
-    for line in fi:
-        if line[0] == '1': # keep 1 station
-            continue
-        weather_dic[line[1]] = line;
-    weather_indexes = dict([(weather_head[i], i) for i in range(len(weather_head))])
+path_train = "train.csv"
+path_test= "test2.csv"
+path_weather="weather.csv"
 
-    return weather_dic, weather_indexes
+def buildLaggedFeatures(df, lag):  # df - dataframe, lag - list of numbers defining lagged values. Builds lagged weather features
+    new_dict={}
+    for col_name in df:
+        new_dict[col_name]=df[col_name]
+        # create lagged Series
+        for l in lag:
+            if col_name!='Date' and col_name!='Station':
+                new_dict['%s_lag%d' %(col_name,l)]=df[col_name].shift(l)
+    res=pd.DataFrame(new_dict,index=df.index)
+    return res
 
-species_map = {'CULEX RESTUANS' : "100000",
-              'CULEX TERRITANS' : "010000",
-              'CULEX PIPIENS'   : "001000",
-              'CULEX PIPIENS/RESTUANS' : "101000",
-              'CULEX ERRATICUS' : "000100",
-              'CULEX SALINARIUS': "000010",
-              'CULEX TARSALIS' :  "000001",
-              'UNSPECIFIED CULEX': "001000"}
+def DuplicatedRows(df): # Calculates number of duplicated rows by Date, Trap, Species
+    grouped = df.groupby(['Date', 'Trap', 'Species'])
+    num=grouped.count().Latitude.to_dict()
+    df['N_Dupl']=-999
+    for idx in df.index:
+        d = df.loc[idx, 'Date']
+        t = df.loc[idx, 'Trap']
+        s = df.loc[idx, 'Species']
+        df.loc[idx, 'N_Dupl'] = num[(d, t, s)]
+    return df
 
-def process_line(line, indexes, weather_dic, weather_indexes):
-    def get(name):
-        return line[indexes[name]]
+def haversine(lat1, lon1, lat2, lon2): # Calculates the haversine distance between two Lat, Long pairs
+    R = 6372.8 # Earth radius in kilometers
+    dLat = np.radians(lat2 - lat1)
+    dLon = np.radians(lon2 - lon1)
+    lat1 = np.radians(lat1)
+    lat2 = np.radians(lat2)
+    a = np.power(np.sin(dLat/2), 2) + np.multiply(np.cos(lat1), np.multiply(np.cos(lat2), np.power(np.sin(dLon/2), 2)))
+    c = 2*np.arcsin(np.sqrt(a))
+    return R * c
 
-    date = get("Date")
-    month = float(date.split('-')[1])
-    week = int(date.split('-')[1]) * 4 + int(date.split('-')[2]) / 7
-    latitude = float(get("Latitude"))
-    longitude = float(get("Longitude"))
-    species=species_map[get("Species")];
-    #traps=get("Trap");
-    #print species, traps
+def ClosestStation(df): # identify the closest weather station
+    df['lat1']=41.995   # latitude of 1st station
+    df['lat2']=41.786   # latitude of 2d station
+    df['lon1']=-87.933  # longitude of 1st station
+    df['lon2']=-87.752  # longitude of 2d station
+    df['dist1'] = haversine(df.Latitude.values, df.Longitude.values, df.lat1.values, df.lon1.values) #calculate distance
+    df['dist2'] = haversine(df.Latitude.values, df.Longitude.values, df.lat2.values, df.lon2.values)
+    indicator = np.less_equal(df.dist1.values, df.dist2.values) # determine which station is the closest
+    st = np.ones(df.shape[0])
+    st[indicator==0]=2
+    df['Station']=st    # obtain station identifier for each row
+    print df['Station']
+    df.drop(['dist1', 'dist2', 'lat1', 'lat2', 'lon1', 'lon2' ], axis=1, inplace=True)
+    return df
 
-    # weather features
-    tmax = float(weather_dic[date][weather_indexes["Tmax"]])
-    tmin = float(weather_dic[date][weather_indexes["Tmin"]])
-    tavg = float(weather_dic[date][weather_indexes["Tavg"]])
-    dewpoint = float(weather_dic[date][weather_indexes["DewPoint"]])
-    wetbulb = float(weather_dic[date][weather_indexes["WetBulb"]])
-    pressure = float(weather_dic[date][weather_indexes["StnPressure"]])
-    temp=(weather_dic[date][weather_indexes["PrecipTotal"]])
-    if (temp.isdigit()):
-        if float(temp)>0:
-            precipitation = 1.0;
-        else:
-            precipitation = 0.0;
-    else:
-        precipitation = 0.0;
-    windspeed = float(weather_dic[date][weather_indexes["ResultSpeed"]])
-    #return [month, week, latitude, longitude, tmax, tmin, tavg, dewpoint, wetbulb, pressure, precipitation, windspeed, species]
+def DateSplit(df):   # parse date in text format to get 3 separate columns for year, month, day
+    df = df.copy()
+    df['Year'] = pd.DatetimeIndex(df['Date']).year
+    df['Month'] = pd.DatetimeIndex(df['Date']).month
+    df['Day'] = pd.DatetimeIndex(df['Date']).day
+    return df
 
-    return [month, week, latitude, longitude, tmax, tmin, tavg, dewpoint, wetbulb, pressure, windspeed, species]
+def GetDummies(df):   # Constructs dummy indicators for species
+    dummies=pd.get_dummies(df['Species'])
+    df = pd.concat([df, dummies], axis=1)
+    return df
 
-def preprocess_data(X, scaler=None):
-    #replace missing values
-    df = pd.DataFrame(X);
-    df = df.replace('T', -1)
-    df = df.replace('M', -1)
-    df = df.replace('-', -1)
-    X = df.as_matrix()
-    #print X[:,10]
-    if not scaler:
-        scaler = StandardScaler()
-        scaler.fit(X)
-    X = scaler.transform(X)
-    return X, scaler
+def ExtendWnv(df):  # assigns WNV indicator to duplicate rows (if WNV=1, assign 1 to all duplicate rows)
+    grouped = df.groupby(by=['Date', 'Trap', 'Species'], as_index=False)['WnvPresent'].max()
+    df.drop('WnvPresent', axis=1, inplace=True)
+    grouped.columns = ['Date', 'Trap', 'Species', 'WnvPresent']
+    result = df.merge(grouped, on=['Date', 'Trap', 'Species'], how="left") #.reset_index()
+    return result
+
+def MergeWeather(df1, df2):
+    result = df1.merge(df2, on=['Date', 'Station'], how="left",  left_index=True)
+    #result = df1.merge(df2, on=['Date'], how="left",  left_index=True)
+
+    return result
+
+def LoadWeather():  # Load and prepare weather data
+    #days=[1];
+    days = [1, 3, 5, 8, 12]     #lagged weather values used as features
+    weather = pd.read_csv(path_weather)
+    weather.sort_values(['Date', 'Station'], axis=0, ascending=True, inplace=True)
+    filter_out = ['Heat', 'CodeSum', 'Depth', 'Water1', 'SnowFall', 'StnPressure',  'SeaLevel', 'AvgSpeed' ]
+    weather.drop(filter_out, axis=1, inplace=True) # these variables are not used in the analysis
+    weather.replace(['  T','M','-'], [0.001, np.nan, np.nan], inplace=True) # replace "Trace" with 0.001, replace M and missing with NaN
+    weather.WetBulb.fillna(method='bfill', inplace=True)  # replace missing WetBulb of 1st station with the value of 2d station
+    weather.fillna(method='pad', inplace=True)   # replace all missing values of 2d station with values of 1st station
+    weather1 = buildLaggedFeatures(weather[weather['Station']==1], days) # build lagged features for 1st station
+    weather2 = buildLaggedFeatures(weather[weather['Station']==2], days) # build lagged features for 2d station
+    weather = weather1.append(weather2)                                  # append data from 2 stations
+    weather.sort_values(['Date', 'Station'], axis=0, ascending=True, inplace=True)
+    weather.fillna(0, inplace=True)
+    return weather
+
+def LoadTest(version): # Load and prepare test data
+    print("Loading Test Data")
+    test = pd.read_csv(path_test)
+    test = DateSplit(test)
+    test = DuplicatedRows(test)
+    if version == 2: # calculate duplicated rows for various subgroups of data
+         l = [['Date', 'Species'], ['Date', 'Trap'], ['Date', 'Block'], ['Year', 'Species'],
+              ['Year', 'Trap'], ['Year', 'Block'], ['Month', 'Species'], ['Month', 'Trap'],
+              ['Month', 'Block']]
+         for i in range(len(l)):
+             test = AddMoreFeatures(test, l[i]) # compute number of duplicated rows for each group
+    test = ClosestStation(test)
+    #print test.count
+    weather = LoadWeather()
+    #print weather.count
+    test = MergeWeather(test, weather)
+    #print test.count
+    test.replace(['UNSPECIFIED CULEX'], ['CULEX PIPIENS'], inplace=True) # replace Unspecified species with PIPIENS
+    test = GetDummies(test)
+    filter_out = ['Address', 'Block', 'Street', 'Trap', 'AddressNumberAndStreet', 'AddressAccuracy',
+                 'Date', 'Species', 'Station' ]
+    test.drop(filter_out, axis=1, inplace=True) # these features are not used in prediction
+    return test
+
+def LoadTrain(version): # Load and prepare train data
+    print("Loading Train Data")
+    train = pd.read_csv(path_train)
+    train = DateSplit(train)
+    train = DuplicatedRows(train)
+    if version == 2: # calculate duplicated rows for various subgroups of data
+         l = [['Date', 'Species'], ['Date', 'Trap'], ['Date', 'Block'], ['Year', 'Species'],
+              ['Year', 'Trap'], ['Year', 'Block'], ['Month', 'Species'], ['Month', 'Trap'],
+              ['Month', 'Block']]
+         for i in range(len(l)):
+             train = AddMoreFeatures(train, l[i]) # compute number of duplicated rows for each group
+    train = ExtendWnv(train)
+    train = ClosestStation(train)
+    weather = LoadWeather()
+    train = MergeWeather(train, weather)
+    train = GetDummies(train)
+    filter_out = ['Address', 'Block', 'Street', 'Trap', 'AddressNumberAndStreet', 'AddressAccuracy',
+                 'Date', 'Species', 'Station']
+    train.drop(filter_out, axis=1, inplace=True) # these features are not used in prediction
+    return train
+
+# Note: This is a POST-DEADLINE improvement
+# This procedure calculates the number of duplicated rows by ['Date', 'Species'], ['Date', 'Trap'], ['Date', 'Block'],
+# ['Year', 'Species'], and etc. This improves LB score by about 0.017-0.018
+def AddMoreFeatures(df, list_cols):
+    indicator = np.not_equal(df.N_Dupl.values, np.ones(df.shape[0]))
+    df['N'] = indicator
+    grouped = df.groupby(by=list_cols, as_index=False)['N'].sum()
+    grouped.columns = [list_cols[0], list_cols[1], 'N_Dupl_%s_%s' %(list_cols[0], list_cols[1])]
+    df.drop('N', axis=1, inplace=True)
+    result = df.merge(grouped, on=list_cols, how="left")
+    return result
 
 def shuffle(X, y, seed=1337):
     np.random.seed(seed)
     shuffle = np.arange(len(y))
     np.random.shuffle(shuffle)
-    X = X[shuffle]
-    y = y[shuffle]
+    X = X.iloc[:,shuffle]
+    y = y.iloc[:,shuffle]
     return X, y
+#######################################
+Data_Version = 2 # 2    Choose the data version (1,2)
 
 
 
-
-# now the actual script
-
-print("Processing training data...")
-rows = []
-labels = []
-fi = csv.reader(open("train.csv"))
-head = fi.next()
-indexes = dict([(head[i], i) for i in range(len(head))])
-weather_dic, weather_indexes = get_weather_data()
-for line in fi:
-    rows.append(process_line(line, indexes, weather_dic, weather_indexes))
-    labels.append(float(line[indexes["WnvPresent"]]))
-
-X = np.array(rows)
-y = np.array(labels)
-
-
-
-
-#Sanity check the data - visualize, find correlations etc
-#df = pd.DataFrame(X[:,0:9])
-#pd.plotting.scatter_matrix(df);
-#plt.show()
-#print df.describe()
-
-print "sample data: [month, week, latitude, longitude, tmax, tmin, tavg, dewpoint, wetbulb, pressure,  windspeed, species]"
-print rows[:10]
-print labels[:10]
-featurelabels = {0 : "month",
-           1 : "week",
-           2 : "latitude",
-           3 : "longitude",
-           4 : "tmax",
-           5 : "tmin",
-           6 : "tavg",
-           7 : "dewpoint",
-           8 : "wetbulb",
-           9 : "pressure",
-           #10: "precipitation",
-           10: "windspeed",
-           11: "species",
-}
-print "Summary statistics for features"
-def sanitycheckData(X, idx):
-    print X[:,idx]
-    df=pd.DataFrame(X[:,idx]);
-    #print [x for x in X[:,0] if x<0]
-    #df.hist()
-    #plt.show()
-    print df.describe()
-
-for idx in xrange(len(featurelabels)):
-    print idx, featurelabels[idx];
-    sanitycheckData(X,idx)
-
-
-# shuffle and preprocess_data
-X, y = shuffle(X, y)
-X, scaler = preprocess_data(X)
-#Y = np_utils.to_categorical(y)
-
-
-print np.count_nonzero(y)*1.0/len(y)
-input_dim = X.shape[1]
-output_dim = 2
+train = LoadTrain(Data_Version)  # load prepared data
+train  = train.astype(float)
+train = train.rename(columns={'CULEX ERRATICUS': 'ERRATICUS', 'CULEX PIPIENS': 'PIPIENS','CULEX PIPIENS/RESTUANS' : 'PIPIENSRESTUANS', 'CULEX RESTUANS': 'RESTUANS',  'CULEX SALINARIUS': 'SALINARIUS', 'CULEX TARSALIS':'TARSALIS',  'CULEX TERRITANS':'TERRITANS'})
+#print train.describe()
+#print train.head
+#train = train.reindex(np.random.permutation(train.index))
+wnv=train['WnvPresent']#.values    # prediction target
+#wnv=train.WnvPresent.values    # prediction target
+#print wnv
+train.drop(['NumMosquitos', 'WnvPresent'], axis=1, inplace=True)
+print train.count
+print train.iloc[:,:10]
+print train.iloc[:,10:20]
+print train.iloc[:,20:30]
+print train.iloc[:,31:40]
+print train.iloc[:,41:60]
+print train.iloc[:,60:80]
+print train.iloc[:,80:94]
+#train, wnv = shuffle(train, wnv)
+#print train.count
+#train=train.iloc[:,:89]
+#test = LoadTest(Data_Version)
+#print test.describe()
+#print test.count
 
 print("Validation...")
 
 nb_folds = 4
-kfolds = KFold(len(y), nb_folds)
+kfolds = KFold(len(wnv), nb_folds)
 av_roc = 0.
 f = 0
 
-"""
-    To train the random forest classifier with features and target data
-    :param features:
-    :param target:
-    :return: trained random forest classifier
-    """
-for train, valid in kfolds:
+print kfolds
+
+
+#    To train the random forest classifier with features and target data
+#    :param features:
+#    :param target:
+#    :return: trained random forest classifier
+
+for trainrows, validrows in kfolds:
     print('---'*20)
     print('Fold', f)
     print('---'*20)
     f += 1
-    X_train = X[train]
-    X_valid = X[valid]
-    y_train = y[train]
-    y_valid = y[valid]
+    train_rows = train.iloc[trainrows,:]
+    train_validation = train.iloc[validrows,:]
+    wnv_train = wnv.iloc[trainrows]
+    wnv_validation = wnv.iloc[validrows]
     clf = RandomForestClassifier(n_estimators=1000, n_jobs=-1);
-    clf.fit(X_train, y_train)
+    clf.fit(train_rows, wnv_train)
     trained_model=clf;
     print "Trained model :: ", trained_model
-    valid_preds = trained_model.predict_proba(X_valid)
+    valid_preds = trained_model.predict_proba(train_validation)
     print valid_preds
 
     valid_preds = valid_preds[:, 1]
-    roc = metrics.roc_auc_score(y_valid, valid_preds)
+    roc = metrics.roc_auc_score(wnv_validation, valid_preds)
     print("ROC:", roc)
     av_roc += roc
 
 print('Average ROC:', av_roc/nb_folds)
 
+
 print("Generating submission...")
 clf = RandomForestClassifier(n_estimators=1000, n_jobs=-1);
-clf.fit(X, y)
+clf.fit(train, wnv)
 trained_model=clf;
 ##################
 #Feature importance
@@ -216,33 +278,45 @@ indices = np.argsort(importances)[::-1]
 # Print the feature ranking
 print("Feature ranking:")
 
-for f in range(X.shape[1]):
-    print("%d. feature %d %s (%f)" % (f + 1, indices[f], featurelabels[indices[f]],importances[indices[f]]))
-print range(X.shape[1]), indices
+#for f in range(X.shape[1]):
+#    print("%d. feature %d %s (%f)" % (f + 1, indices[f], featurelabels[indices[f]],importances[indices[f]]))
+print range(train.shape[1]), indices
 # Plot the feature importances of the forest
 plt.figure()
 plt.title("Feature importances")
-plt.bar(range(X.shape[1]), importances[indices],
+plt.bar(range(train.shape[1]), importances[indices],
        color="b", yerr=std[indices], align="center")
-plt.xticks(range(X.shape[1]), [featurelabels[k] for k in indices])
-plt.xlim([-1, X.shape[1]])
+plt.xticks(range(train.shape[1]),  indices)
+plt.xlim([-1, train.shape[1]])
 plt.show()
-###########################################
-# Generate predictions
-fi = csv.reader(open("test.csv"))
-head = fi.next()
-indexes = dict([(head[i], i) for i in range(len(head))])
-rows = []
-ids = []
-for line in fi:
-    rows.append(process_line(line, indexes, weather_dic, weather_indexes))
-    ids.append(line[0])
-X_test = np.array(rows)
-X_test, _ = preprocess_data(X_test, scaler)
 
-preds = trained_model.predict_proba(X_test)
+###########
+test = LoadTest(Data_Version)
+#print test.describe()
+ids=test.Id.values
+test.drop(['Id'], axis=1, inplace=True)
+test  = test.astype(float)
+test = test.rename(columns={'CULEX ERRATICUS': 'ERRATICUS', 'CULEX PIPIENS': 'PIPIENS','CULEX PIPIENS/RESTUANS' : 'PIPIENSRESTUANS', 'CULEX RESTUANS': 'RESTUANS',  'CULEX SALINARIUS': 'SALINARIUS', 'CULEX TARSALIS':'TARSALIS',  'CULEX TERRITANS':'TERRITANS'})
+print test.count
 
-fo = csv.writer(open("randomforestoutput.csv", "w"), lineterminator="\n")
+print test.iloc[:,:10]
+print test.iloc[:,10:20]
+print test.iloc[:,20:30]
+print test.iloc[:,31:40]
+print test.iloc[:,41:60]
+print test.iloc[:,60:80]
+print test.iloc[:,80:95]
+
+
+#test = LoadTest(Data_Version)
+#print test.describe()
+
+#test  = test.astype(float)
+#test = test.rename(columns={'CULEX ERRATICUS': 'ERRATICUS', 'CULEX PIPIENS': 'PIPIENS','CULEX PIPIENS/RESTUANS' : 'PIPIENSRESTUANS', 'CULEX RESTUANS': 'RESTUANS',  'CULEX SALINARIUS': 'SALINARIUS', 'CULEX TARSALIS':'TARSALIS',  'CULEX TERRITANS':'TERRITANS'})
+#print test.count
+preds = trained_model.predict_proba(test)
+print preds
+fo = csv.writer(open("randomforestoutput2.csv", "w"), lineterminator="\n")
 fo.writerow(["Id","WnvPresent"])
 for i, item in enumerate(ids):
     fo.writerow([ids[i], preds[i][1]])
